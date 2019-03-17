9cb73873516e21465806436bd49fe22a036f3cf0
chengcongyuegithub <chengcongyue@sina.com>
2019-03-17 15:26:56 +0800

    flask项目第四天之图片的上传以及评论的添加

--- nowstagram/nowstagram/views.py
+++ nowstagram/nowstagram/views.py
@@ -1,12 +1,14 @@
 #-*- encoding=UTF-8 -*-
 
 from nowstagram import app, db
-from models import Image, User
-from flask import render_template, redirect, request, flash, get_flashed_messages
+from models import Image, User ,Comment
+from flask import render_template, redirect, request, flash, get_flashed_messages,send_from_directory
 from flask_login import login_user,logout_user,login_required,current_user
 import hashlib
 import random
 import json
+import uuid
+import os
 
 @app.route('/')
 def index():
@@ -27,17 +29,19 @@ def profile(user_id):
     user = User.query.get(user_id)
     if user == None:
         return redirect('/')
-    paginate = Image.query.paginate(page=1, per_page=3)
-    return render_template('profile.html', user = user,has_next=paginate.has_next,images = paginate.items)
+    paginate = Image.query.filter_by(user_id=user_id).order_by(db.desc(Image.id)).paginate(page=1, per_page=3, error_out=False)
+    return render_template('profile.html', user=user, images=paginate.items, has_next=paginate.has_next)
 
-@app.route('/profile/images/<int:user_id>/<int:per_page>/')
-def user_images(user_id,page,per_page):
-    paginate = Image.query.filter_by(user_id=user_id).paginate(page=page, per_page=per_page)
+
+@app.route('/profile/images/<int:user_id>/<int:page>/<int:per_page>/')
+def user_images(user_id, page, per_page):
+    paginate = Image.query.filter_by(user_id=user_id).order_by(db.desc(Image.id)).paginate(page=page, per_page=per_page, error_out=False)
     map = {'has_next': paginate.has_next}
     images = []
     for image in paginate.items:
         imgvo = {'id': image.id, 'url': image.url, 'comment_count': len(image.comments)}
         images.append(imgvo)
+
     map['images'] = images
     return json.dumps(map)
 
@@ -103,3 +107,38 @@ def redirect_with_msg(target,msg,category):
     if msg != None:
         flash(msg,category=category)
     return redirect(target)
+
+def save_to_local(file,file_name):
+    save_dir=app.config['UPLOAD_DIR']
+    file.save(os.path.join(save_dir,file_name))
+    return '/image/'+file_name
+
+@app.route('/upload/',methods={'post'})
+@login_required
+def upload():
+    file=request.files['file']
+    file_ext=''
+    if file.filename.find('.') > 0:
+        file_ext = file.filename.rsplit('.', 1)[1].strip().lower()
+    if file_ext in app.config['ALLOWED_EXT']:
+        file_name = str(uuid.uuid1()).replace('-', '') + '.' + file_ext
+        url=save_to_local(file,file_name)
+        if url !=None:
+            db.session.add(Image(url,current_user.id))
+            db.session.commit()
+    return redirect('/profile/%d' % current_user.id)
+
+@app.route('/image/<image_name>')
+def view_image(image_name):
+    return send_from_directory(app.config['UPLOAD_DIR'],image_name)
+
+@app.route('/addcomment/',methods={'post'})
+@login_required
+def add_comment():
+    image_id=int(request.values['image_id'])
+    content=request.values['content']
+    comment= Comment(content,image_id,current_user.id)
+    db.session.add(comment)
+    db.session.commit()
+    return json.dumps({"code":0,"id":comment.id,"content":comment.content,
+                       "username":comment.user.username,"user_id":comment.user_id})
